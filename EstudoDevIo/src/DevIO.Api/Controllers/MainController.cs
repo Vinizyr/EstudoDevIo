037c6adfc61eba47e0a71d826556e4e4bd81decd
Marcos Vinicios Araujo Santos <marcosvinicios@seias.gov>
2025-03-19 16:26:16 -0300

    Estudo Desenvolvedor.io

---
+++ EstudoDevIo/src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        // validacao de notificacoes de erro
+
+        // validacao de modelstate
+
+        //validacao de operacao de negócios
+    }
+
+    [Route("api/[controller]")]
+    public class FornecedoresController : MainController
+    {
+
+    }
+}
